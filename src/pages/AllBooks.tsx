b67dd0bb4ec92a6d1e7174b61137a4f2482041f6
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-20 03:47:51 +0600

    get books added and search field and filter button added

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,6 +1,8 @@
 import BookCard from "@/components/BookCard";
+import { Input } from "@/components/ui/input";
 import { useGetBooksQuery } from "@/redux/features/Books/booksApi";
 import { IBook } from "@/types/globalTypes";
+import { RiFilter3Fill } from "react-icons/ri";
 
 const AllBooks = () => {
   const { data, isLoading, isError } = useGetBooksQuery(undefined, {
@@ -9,7 +11,17 @@ const AllBooks = () => {
   });
 
   return (
-    <section className="container mx-auto">
+    <section className="container mx-auto my-10">
+      <div className="flex justify-end items-center">
+        <Input
+          className="w-2/4"
+          placeholder="Search by author name, title, or genre"
+        />
+        <div className="border p-1 ml-3 rounded-lg">
+          <RiFilter3Fill className="text-3xl cursor-pointer" />
+        </div>
+      </div>
+
       <div className="grid grid-cols-4 gap-4 my-8">
         {data?.data.map((book: IBook) => (
           <BookCard booksData={book} />
